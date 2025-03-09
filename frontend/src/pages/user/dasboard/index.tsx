e59a80ad59d3562cbac50eac6a3e52adf88139bb
Theshawa Dasun <das@synrgise.com>
2025-03-09 19:59:56 +0530

    asdsda

--- frontend/src/pages/user/dasboard/index.tsx
+++ frontend/src/pages/user/dasboard/index.tsx
@@ -1,5 +1,48 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  Grid2,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { FC } from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../../auth/useAuth";
 
 export const UserDashboardPage: FC = () => {
-  return <>User Dashboard</>;
+  const { auth } = useAuth();
+  return (
+    <Stack spacing={6}>
+      <Typography variant="h2">Hello {auth?.name}!</Typography>
+      <Grid2 container spacing={2}>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4 }}>
+          <Card>
+            <Link to={`/user/teams`}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="div">
+                    View Teams
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Link>
+          </Card>
+        </Grid2>
+        <Grid2 size={{ xs: 12, sm: 6, md: 4 }}>
+          <Card>
+            <Link to={`/user/player-categories`}>
+              <CardActionArea>
+                <CardContent>
+                  <Typography gutterBottom variant="h6" component="div">
+                    View Players
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Link>
+          </Card>
+        </Grid2>
+      </Grid2>
+    </Stack>
+  );
 };
